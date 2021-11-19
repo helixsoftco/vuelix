e82aa9ab3b7198a2a27be3859a2039bbf2e1915c
Edison Arango <edisonarab@gmail.com>
2021-11-19 14:35:24 -0500

    configured layouts in vite and router

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
-import Pages from 'vite-plugin-pages'
 import vue from '@vitejs/plugin-vue'
+import Pages from 'vite-plugin-pages'
+import Layouts from 'vite-plugin-vue-layouts'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Pages()],
+  plugins: [vue(), Pages(), Layouts()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
