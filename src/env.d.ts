31e51d1e9d78353959a78913dcdec87fa82145e6
Edison Arango <edisonarab@gmail.com>
2021-11-19 14:41:21 -0500

    Feature/layouts system (#2)
    * installed vite plugin vue layouts
    
    * configured layouts in vite and router
    
    * create default layout
    
    * create main layout and using it in home
    
    * add feature to README

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pages/client" />
+/// <reference types="vite-plugin-vue-layouts/client" />
 
 declare module '*.vue' {
   import { DefineComponent } from 'vue'
--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router"
 import generatedRoutes from "~pages"
+import { setupLayouts } from 'virtual:generated-layouts'
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: generatedRoutes,
+  routes: setupLayouts(generatedRoutes),
 })
 
 export default router
