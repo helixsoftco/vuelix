b0a62f2fa1d3186e46033bc86c7d3c343b7dcbc0
Edison Arango <edisonarab@gmail.com>
2021-11-19 14:35:57 -0500

    create main layout and using it in home

---
+++ src/layouts/main.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav>
+    <strong>PROJECT NAME</strong>
+    <div>
+      Hello Developer!
+    </div>
+  </nav>
+
+  <main>
+    <router-view></router-view>
+  </main>
+</template>
+
+<style>
+nav {
+  display: flex;
+  border-bottom: 1px solid #dcdcdc;
+  padding: 10px;
+  justify-content: space-between;
+}
+</style>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,3 +1,8 @@
+<route lang="yaml">
+meta:
+  layout: main
+</route>
+
 <template>
   <h1>Home Page</h1>
   <p>This page correspond to the "/home" URL generated from this file name and location</p>
