eca01077a6c36c2bb8c9ee53138b93468d384ec4
Edison Arango <edisonarab@gmail.com>
2021-11-19 22:09:53 -0500

    add profile page

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -7,5 +7,7 @@ meta:
   <h1>Home Page</h1>
   <p>This page correspond to the "/home" URL generated from this file name and location</p>
 
+  <router-link :to="{ name: 'profile' }">To Profile</router-link>
+  <br>
   <router-link :to="{ name: 'index' }">To Index</router-link>
 </template>
---
+++ src/pages/profile.vue
@@ -0,0 +1,13 @@
+<route lang="yaml">
+meta:
+  layout: main
+</route>
+
+<template>
+  <h1>Profile Page</h1>
+  <p>This is the profile page</p>
+
+  <router-link :to="{ name: 'home' }">To Home</router-link>
+  <br>
+  <router-link :to="{ name: 'index' }">To Index</router-link>
+</template>
