28a5e8051c558be0ba32886cc64e19f9cce3e0ba
Edison Arango <edisonarab@gmail.com>
2021-11-23 22:33:44 -0500

    installed and configured eslint-config-prettier and eslint-plugin-prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,20 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'plugin:vue/essential', 'plugin:@typescript-eslint/recommended'],
+  // The "prettier" extend comes from 'eslint-config-prettier' which disables eslint rules that may conflict with prettier
+  extends: ['eslint:recommended', 'plugin:vue/essential', 'plugin:@typescript-eslint/recommended', 'prettier'],
   parserOptions: {
     ecmaVersion: 13,
     parser: '@typescript-eslint/parser',
     sourceType: 'module',
   },
-  plugins: ['vue', '@typescript-eslint'],
-  rules: {},
+  /**
+   * The "prettier" plugin comes from 'eslint-plugin-prettier' which integrates prettier rules into eslint rules,
+   * this plugins is what allows to display a warn directly in the editor for related prettier rules
+   * See: https://github.com/prettier/eslint-plugin-prettier#recommended-configuration
+   */
+  plugins: ['vue', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': ['warn'],
+  },
 }
