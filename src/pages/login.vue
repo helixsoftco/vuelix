27ae7ee0e13e6cbc204310685d2ddcddcac01155
Edison Arango <edisonarab@gmail.com>
2021-11-26 17:30:44 -0500

    add login page

---
+++ src/pages/login.vue
@@ -0,0 +1,10 @@
+<template>
+  <h2>Login Page</h2>
+  <p v-if="$route.query.redirectTo" style="color: brown">You need to be Authenticated in order to see this page</p>
+  <button @click="$auth.login">Login</button>
+
+  <br /><br />
+  <router-link :to="{ name: 'index' }">To Index</router-link>
+</template>
+
+<script setup lang="ts"></script>
