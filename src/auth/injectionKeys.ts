f67c23f04513626e01c51e1ea24520644c29c014
Edison Arango <edisonarab@gmail.com>
2021-11-26 17:25:25 -0500

    remove injection keys

--- src/auth/injectionKeys.ts
+++
@@ -1,5 +0,0 @@
-import { InjectionKey } from 'vue'
-import { AuthPlugin } from './types'
-
-// See: https://v3.vuejs.org/api/composition-api.html#provide-inject
-export const authInjectionKey: InjectionKey<AuthPlugin> = Symbol()
