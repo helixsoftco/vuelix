1c82c6c6b52fc6f18734fd6af251109ccc7c0122
Edison Arango <edisonarab@gmail.com>
2021-11-26 11:16:48 -0500

    install plugin

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,14 @@ import '@/assets/scss/app.scss'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createAuth } from './auth'
+
+const auth = createAuth({
+  logoutRedirectRoute: { name: 'index' },
+})
 
 const app = createApp(App)
 app.use(router)
+app.use(auth)
 app.provide('enable-route-transitions', true)
 app.mount('#app')
