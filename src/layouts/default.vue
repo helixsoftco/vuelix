a16a06f57ea1974b2bf00f0efc90d0b4a6781bfd
Edison Arango <edisonarab@gmail.com>
2021-11-26 17:30:20 -0500

    default layout using auth composition API

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,8 +1,21 @@
 <script setup lang="ts">
+import { useAuth } from '@/auth'
 import RouterViewTransition from '@/components/RouterViewTransition.vue'
+
+// Composition API example
+let auth = useAuth()
 </script>
 
 <template>
   <RouterViewTransition></RouterViewTransition>
   <p>[Default Layout]</p>
+
+  <p>
+    <span v-if="auth.isAuthenticated">
+      Authenticated as <b>{{ auth.user.email }}</b
+      >&nbsp;
+      <button @click="auth.logout">Logout</button>
+    </span>
+    <b v-else>Not Authenticated</b>
+  </p>
 </template>
