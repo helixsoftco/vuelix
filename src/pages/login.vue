9d7cb3f5be17795533ebc81e30994712f8078987
Edison Arango <edisonarab@gmail.com>
2021-11-29 07:52:25 -0500

    Feature/bootstrap (#9)
    * install bootstrap 5.1
    
    * create variables
    
    * load bootstrap
    
    * using bootstrap in buttons and using margin utilities

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,7 +1,7 @@
 <template>
-  <h1>Login Page</h1>
+  <h1 class="mt-100">Login Page</h1>
   <p v-if="$route.query.redirectTo" style="color: brown">You need to be Authenticated in order to see this page</p>
-  <button @click="$auth.login">Login with Auth Plugin</button>
+  <button class="btn btn-success" @click="$auth.login">Login with Auth Plugin</button>
 
   <br /><br />
   <router-link :to="{ name: 'index' }">To Index</router-link>
