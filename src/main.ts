c318f64044b7244b7794bac98b7499f22c13d379
Edison Arango <edisonarab@gmail.com>
2021-11-30 00:20:09 -0500

    config i18n autoloading files from locales dir

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { createAuth } from './auth'
 import App from './App.vue'
 import router from './router'
 import axiosInstance from './api/axios'
+import i18n from './plugins/i18n'
 
 const auth = createAuth({
   router,
@@ -19,5 +20,6 @@ const auth = createAuth({
 const app = createApp(App)
 app.use(router)
 app.use(auth)
+app.use(i18n)
 app.provide('enable-route-transitions', true)
 app.mount('#app')
