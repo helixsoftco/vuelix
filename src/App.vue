9da7b055616cdeac83a0a3baa2fef0fe3484430f
Johan León <johan.leon@unillanos.edu.co>
2022-09-09 18:23:58 -0500

    Feature/add toasts component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import RouterViewTransition from '@/components/RouterViewTransition.vue'
+import Toasts from '@/components/Toasts.vue'
 </script>
 
 <template>
   <RouterViewTransition is-root></RouterViewTransition>
+  <Toasts />
 </template>
