7ef84532d221e4b8e849f24601e585b8f1c6075a
Johan León <johan.leon@unillanos.edu.co>
2022-09-12 12:09:56 -0500

    Feature/add withLoading components

---
+++ src/components/loading/Loading.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="text-center mt-3">
+    <div class="spinner-border text-center" role="status">
+      <span class="visually-hidden">{{ $t('Loading...') }}</span>
+    </div>
+  </div>
+</template>
---
+++ src/components/loading/WithLoadingIcon.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+export default {
+  inheritAttrs: false,
+}
+</script>
+
+<script setup lang="ts">
+interface Props {
+  loading: boolean
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <div v-if="loading" v-bind="$attrs" class="spinner-grow" role="status" style="width: 1.2rem; height: 1.2rem"></div>
+  <slot v-else>
+    <i-bi-circle v-bind="$attrs"></i-bi-circle>
+  </slot>
+</template>
