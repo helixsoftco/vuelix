6dff2285499caa7f1d01bf9799c8b3699bd19c46
Edison Arango <edisonarab@gmail.com>
2023-04-21 03:29:33 -0500

    Feature/upgrade toasts

--- src/composables/useToasts.ts
+++ src/composables/useToasts.ts
@@ -1,4 +1,4 @@
-import { ref } from 'vue'
+import { ref, VNode } from 'vue'
 import { useI18n } from 'vue-i18n'
 
 const DEFAULT_TIMEOUT = 5000
@@ -7,25 +7,37 @@ type BSVariant = 'primary' | 'secondary' | 'danger' | 'warning' | 'success' | 'i
 type DefaultVariants = 'danger' | 'success'
 export interface ToastOptions {
   id?: string
-  text: string
+  text?: string
   variant?: BSVariant
   timeout?: number
   customHTML?: string
+  customVNode?: VNode
 }
-export type Toast = Required<ToastOptions>
+export interface Toast extends ToastOptions {
+  // These are optional in the options because we can provide default values from them, but when adding the Toast these values should be set
+  id: string
+  variant: BSVariant
+  timeout: number
+}
 
 const toasts = ref<Toast[]>([])
 
+export function addToast({
+  text,
+  variant = 'primary',
+  timeout = DEFAULT_TIMEOUT,
+  customHTML,
+  customVNode,
+}: ToastOptions) {
+  pushToast({ id: `toast-${Date.now()}`, text, variant, timeout, customHTML, customVNode })
+}
+
+function pushToast(toast: Toast) {
+  toasts.value = [...toasts.value, toast]
+}
+
 export default function useToasts() {
   const { t } = useI18n()
-
-  function pushToast(toast: Toast) {
-    toasts.value = [...toasts.value, toast]
-  }
-
-  function addToast({ text, variant = 'primary', timeout = DEFAULT_TIMEOUT, customHTML = '' }: ToastOptions) {
-    pushToast({ id: `toast-${Date.now()}`, text, variant, timeout, customHTML })
-  }
 
   function addDefaultToast(variant: DefaultVariants) {
     let text = ''
@@ -37,7 +49,12 @@ export default function useToasts() {
         text = t('An unexpected error happened')
         break
     }
-    pushToast({ id: `toast-${Date.now()}`, text, variant, timeout: DEFAULT_TIMEOUT, customHTML: '' })
+    pushToast({
+      id: `toast-${Date.now()}`,
+      text,
+      variant,
+      timeout: DEFAULT_TIMEOUT,
+    })
   }
 
   function clearToast(id: string) {
